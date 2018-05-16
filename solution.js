22a4c316a5ce11bf54f8bb0025c87b0be8685a2f
dleufer <dleufer@gmail.com>
2018-05-16 16:08:26 +0200

    tests added

--- solution.js
+++ solution.js
@@ -1,3 +1,11 @@
 // Your code here
-
+function isObjectLike(value) {
+  let returner = false
+    if (typeof(value) == 'object') {
+      if (value !== null) {
+        returner = true;
+      }
+    }
+    return returner
+}
 // Be sure it's the cleanest code you can write!
--- tests/test-cases.js
+++ tests/test-cases.js
@@ -2,8 +2,17 @@ tests({
 
   // Fill this in with your test cases
 
-  '': function() {
-    eq("", your_solution(""));
+  'object literal': function() {
+    eq("true", isObjectLike({}));
+  }
+  'array': function() {
+    eq("true", isObjectLike([1, 2, 3]));
+  }
+  'non-object': function() {
+    eq("false", isObjectLike(Function));
+  }
+  'null test': function() {
+    eq("false", isObjectLike(null));
   }
 
 });
